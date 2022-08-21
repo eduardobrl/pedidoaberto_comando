b6b23fb1db732b2d44ea5fc39898e96bdbd2c718
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-08-21 00:29:27 -0300

    adiciona api gateway

--- infra/main.tf
+++ infra/main.tf
@@ -47,6 +47,81 @@ resource "aws_lambda_function" "lambda" {
   source_code_hash = filebase64sha256("app.zip")
 }
 
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "${var.app_name}-${var.microservice_name}-api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "{any+}"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id          = aws_api_gateway_rest_api.api.id
+  resource_id          = aws_api_gateway_resource.resource.id
+  http_method          = "ANY"
+  authorization        = "COGNITO_USER_POOLS"
+  authorizer_id        = aws_api_gateway_authorizer.api_authorizer.id
+  authorization_scopes = ["aws.cognito.signin.user.admin"]
+}
+
+resource "aws_api_gateway_integration" "lambda_function_gtw_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api_handler.invoke_arn
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_api_gateway_rest_api.api.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "api_gw" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_rest_api.api.id,
+      aws_api_gateway_authorizer.api_authorizer.id,
+      aws_api_gateway_integration.lambda_function_gtw_integration.id,
+    ]))
+  }
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_authorizer" "api_authorizer" {
+  name        = "${var.app_name}-${var.microservice_name}-fornecedor"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [data.aws_ssm_parameter.sellbridge_cognito_usuario_user_pool_arn.value]
+}
+
+resource "aws_api_gateway_stage" "dev_stage" {
+  deployment_id = aws_api_gateway_deployment.api_gw.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
+}
+
 # IAM
 resource "aws_iam_role" "role" {
   name = "myrole"
