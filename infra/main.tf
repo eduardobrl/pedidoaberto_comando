cb9e83c34a38b9b9fd7323657fec1442bf98fe8f
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-08-21 00:37:55 -0300

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -30,7 +30,7 @@ terraform {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
+  function_name = aws_lambda_function.api_handler.function_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
