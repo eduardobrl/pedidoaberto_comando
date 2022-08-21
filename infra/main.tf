03eda9c485fbf659b4cf2fc2261f02e04097a13e
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-08-21 01:09:33 -0300

    remove aws_lambda_permission

--- infra/main.tf
+++ infra/main.tf
@@ -31,16 +31,6 @@ provider "aws" {
 
 
 # Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.api_handler.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
-}
-
 resource "aws_lambda_function" "api_handler" {
   filename      = "app.zip"
   function_name = "mylambda"
