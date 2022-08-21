4a9395524cf3ab36907c0819a8b8c4d54ce147ce
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-08-20 23:01:28 -0300

    Teste

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "pedidoaberto"
+
+    workspaces {
+      name = "pedidoaberto_comando"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
 
 # Lambda
 resource "aws_lambda_permission" "apigw_lambda" {
