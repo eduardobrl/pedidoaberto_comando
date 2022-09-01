409ae0c38340aed9c3700d214fffd0a6b6dd1563
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-09-01 12:06:13 -0300

    Teste

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,4 +2,8 @@ import express, { Express, Handler, Request, Response } from 'express';
 
 const app: Express = express();
 
+app.post('/', (req: Request, res: Response) => {
+    res.send('POST Express + TypeScript Server');
+});
+
 export default app;
