e9045c2ed72bedc2241f0f588b3a7e2856c442ab
eduardobrl <eduardobrl10@gmail.com>
2022-09-07 13:34:17 -0300

    test

--- app/src/infra/prisma/prisma_connector.ts
+++ app/src/infra/prisma/prisma_connector.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".prisma/client"
+import { PrismaClient } from "/opt/nodejs/node_modules/.prisma/client"
 import { SecretsManager, SSM } from "aws-sdk"
 import Contants  from '../../domain/constants/constants'
 let prismaClient : PrismaClient
